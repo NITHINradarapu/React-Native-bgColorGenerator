10c64d2578d89e9bce74f980efdc380cb05f7a93
NITHINradarapu <radarapunithin05@gmail.com>
2026-02-07 22:13:33 +0530

    BackgroundColor Generator

--- App.tsx
+++ App.tsx
@@ -1,45 +1,52 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  StatusBar,
+} from 'react-native';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+import {useState} from 'react';
 
+export default function App() {
+  const [randombackground, setrandombackground] = useState("#ffffff");
+
+  const generateColor = () => {
+    const hexaRagne = "0123456789ABCDEF";
+    let color = "#";
+    for(let i=0;i<6;i++){
+      color += hexaRagne[Math.floor(Math.random() * 16)];
+    }
+    setrandombackground(color);
+  }
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
+    <>
+      <StatusBar backgroundColor={randombackground} />
+      <View style={[styles.container, {backgroundColor: randombackground}]}>
+        <TouchableOpacity onPress={generateColor} style={styles.button}>
+          <Text style={styles.buttonText}>Generate Random Color</Text>
+        </TouchableOpacity>
+      </View> 
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: '#000',
+    padding: 20,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
   },
 });
-
-export default App;
